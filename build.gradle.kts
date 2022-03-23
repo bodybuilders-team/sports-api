0a1827efff20eb05bbe5f6c3367d6b7706b7721f
André Páscoa <andre@pascoa.org>
2022-03-23 10:36:01 +0000

    Added dependencies to gradle.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "1.6.10"
+    kotlin("plugin.serialization") version "1.6.10"
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
 }
 
 repositories {
@@ -8,6 +10,11 @@ repositories {
 
 dependencies {
     implementation(group = "org.postgresql", name = "postgresql", version = "42.+")
+    implementation(group = "org.http4k", name = "http4k-core", version = "4.20.2.0")
+    implementation(group = "org.http4k", name = "http4k-server-jetty", version = "4.20.2.0")
+    implementation(group = "org.jetbrains.kotlinx", name = "kotlinx-serialization-json", version = "1.3.2")
+    implementation(group = "org.jetbrains.kotlinx", name = "kotlinx-datetime", version = "0.3.2")
+    runtimeOnly(group = "org.slf4j", name = "slf4j-simple", version = "2.0.0-alpha6")
     testImplementation(kotlin("test"))
 }
 
